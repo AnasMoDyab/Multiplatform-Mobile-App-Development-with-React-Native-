751d782fd05754fa38f05b606a73583b415a9c1e
AnasMoDyab <anasdyab89@gmail.com>
2021-02-02 14:07:54 +0100

    navigatin part 1

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
+class Menu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
+    static navigationOptions = {
+        title: 'Menu'
+    };
 
-function Menu(props) {
+    render() {
 
-    const renderMenuItem = ({item, index}) => {
+        const { navigate } = this.props.navigation;
+       const renderMenuItem = ({item, index}) => {
 
         return (
             <ListItem
@@ -12,7 +24,7 @@ function Menu(props) {
                 title={item.name}
                 subtitle={item.description}
                 hideChevron={true}
-                onPress={() => props.onPress(item.id)}
+                onPress={() => navigate('Dishdetail', { dishId: item.id })}
                 leftAvatar={{ source: require('./images/uthappizza.png')}}
             />
 
@@ -21,12 +33,12 @@ function Menu(props) {
 
     return (
         <FlatList
-            data={props.dishes}
+            data={this.state.dishes}
             renderItem={renderMenuItem}
             keyExtractor={item => item.id.toString()}
         />
     );
 }
-
+}
 
 export default Menu;
