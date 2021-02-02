6dbf60295ce6d2aae69cd098ad627c77a40b25b0
AnasMoDyab <anasdyab89@gmail.com>
2021-02-02 13:15:06 +0100

    Component part 2

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -12,8 +12,10 @@ function Menu(props) {
                 title={item.name}
                 subtitle={item.description}
                 hideChevron={true}
+                onPress={() => props.onPress(item.id)}
                 leftAvatar={{ source: require('./images/uthappizza.png')}}
             />
+
         );
     };
 
