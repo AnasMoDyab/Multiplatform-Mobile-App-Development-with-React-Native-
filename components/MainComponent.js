d7c30962a207637a33fe62a4bae4f842081e2572
AnasMoDyab <anasdyab89@gmail.com>
2021-02-04 13:43:59 +0100

    Form

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -9,7 +9,7 @@ import About from "./AboutComponent";
 import Contact from "./ContactComponent";
 import { connect } from 'react-redux';
 import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
-
+import Reservation from "./ReservationComponent";
 
 
 const styles = StyleSheet.create({
@@ -124,6 +124,23 @@ const ContactNavigator = createStackNavigator({
                           onPress={ () => navigation.toggleDrawer() } />
     })
 });
+
+const ReservationNavigator = createStackNavigator({
+    Reservation: { screen: Reservation }
+}, {
+    navigationOptions: ({ navigation }) => ({
+        headerStyle: {
+            backgroundColor: "#512DA8"
+        },
+        headerTitleStyle: {
+            color: "#fff"
+        },
+        headerTintColor: "#fff",
+        headerLeft: <Icon name="menu" size={24}
+                          iconStyle={{ color: 'white' }}
+                          onPress={ () => navigation.navigate('DrawerToggle') } />
+    })
+})
 const CustomDrawerContentComponent = (props) => (
     <ScrollView>
         <SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
@@ -202,6 +219,21 @@ const MainNavigator = createDrawerNavigator({
                 ),
             },
         },
+    Reservation:
+        { screen: ReservationNavigator,
+            navigationOptions: {
+                title: 'Reserve Table',
+                drawerLabel: 'Reserve Table',
+                drawerIcon: ({ tintColor, focused }) => (
+                    <Icon
+                        name='cutlery'
+                        type='font-awesome'
+                        size={24}
+                        iconStyle={{ color: tintColor }}
+                    />
+                ),
+            }
+        }
 
 
 }, {
